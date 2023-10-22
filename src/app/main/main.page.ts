5b40df79b1cd3c39a5091dc95bb2cd95affd7683
Manuel Brun <m_brun@msn.com>
2023-10-22 22:17:34 +0200

    payload configurable

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { PhotoService } from '../services/photo.service';
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Geolocation, Position } from '@capacitor/geolocation';
+import { Geolocation } from '@capacitor/geolocation';
 import { FormsModule } from '@angular/forms';
-import { CapacitorHttp, HttpResponse } from '@capacitor/core';
+import { HttpClient } from '@angular/common/http';
 
 enum API {
   Infoflora = 0,
   WSL = 1,
   Local = 2
+}
+
+enum Attribute {
+  Images = "Images",
+  LatLon = "Latitude / Longitude",
+  Date = "Date",
+  NumTaxonId = "Num. Taxon ID",
+  ReqTaxonId = "Req. Taxon ID"
 }
 
 @Component({
@@ -22,10 +29,13 @@ enum API {
 })
 export class MainPage {
   API = API;
-    
+  Attribute = Attribute;
+
   //  https -> android:networkSecurityConfig="@xml/network_security_config"
 
   public response: string = "";
+  public attributesAll: string[] = [];
+  public attributesSelected: string[] = [];
   public num_taxon_id: number = 5;
   public req_taxon_id: number = 1046220;
   public api: API = API.Infoflora;
@@ -33,10 +43,17 @@ export class MainPage {
   public urlInfoflora: string = "https://florid.infoflora.ch/api/v1/public/identify/images";
   public urlWsl: string = "https://florid.infoflora.ch/api/v1/public/identify/images";
   public url: string = this.urlInfoflora;
+  public date: string = "";
   public requestInProgress: boolean = false;
-  
-  constructor(public photoService: PhotoService, private http: HttpClient) {
+
+  constructor(public photoService: PhotoService, private http: HttpClient) { }
+
+  ngOnInit() {
     this.updateCoords();
+    this.attributesAll = Object.values(Attribute);
+    this.attributesSelected = this.attributesAll;
+    this.setDate();
+    console.log(this.attributesAll);
   }
 
   addPhotoToGallery() {
@@ -46,30 +63,34 @@ export class MainPage {
   }
 
   async submit() {
-    const body = {
-      images: /*["https://picsum.photos/20"], */ this.photoService.base64Photos,
-      lat: this.photoService.currentLat,
-      lon: this.photoService.currentLon,
-      date: this.getDate(),
-      num_taxon_ids: this.num_taxon_id,
-      req_taxon_ids: [this.req_taxon_id]
-    };
+    this.setDate();
+    const body: any = {};
+    if (this.attributesSelected.includes(Attribute.Images)) body.images = this.photoService.base64Photos;
+    if (this.attributesSelected.includes(Attribute.LatLon)) body.lat = this.photoService.currentLat;
+    if (this.attributesSelected.includes(Attribute.LatLon)) this.photoService.currentLon;
+    if (this.attributesSelected.includes(Attribute.Date)) body.date = this.date;
+    if (this.attributesSelected.includes(Attribute.NumTaxonId)) body.num_taxon_ids = this.num_taxon_id;
+    if (this.attributesSelected.includes(Attribute.ReqTaxonId)) body.req_taxon_ids = [this.req_taxon_id];
     console.log(body);
 
-    let requestUrl = [API.Infoflora, API.WSL].includes(this.api) ? 'https://corsproxy.io/?' + encodeURIComponent(this.url) : this.url;
+    let requestUrl = [API.Infoflora, API.WSL].includes(this.api) ?
+      'https://corsproxy.io/?' + encodeURIComponent(this.url) : this.url;
 
-    const options = {
-      url: requestUrl,
-      headers: {'Content-Type': 'application/json'},
-      data: body,
-    };
-  
+    let headers = { 'Content-Type': 'application/json' };
     this.requestInProgress = true;
-    const response: HttpResponse = await CapacitorHttp.post(options);
-    this.requestInProgress = false;
+    this.http.post<any>(requestUrl, body, { headers }).subscribe({
+      next: data => {
+        this.response = JSON.stringify(data, undefined, 2);
+        this.requestInProgress = false;
+      },
+      error: error => {
+        this.response = JSON.stringify(error, undefined, 2);
+        this.requestInProgress = false;
+        console.error('There was an error!', error);
+        alert('There was an error. Check response output.');
+      }
+    })
 
-    this.response = JSON.stringify(response, undefined, 2); 
-    console.log(response);
   }
 
   clear() {
@@ -83,23 +104,19 @@ export class MainPage {
     console.log(e);
     const api = (e as CustomEvent).detail.value;
 
-    switch(api) { 
-      case API.Infoflora: { 
-         this.url = this.urlInfoflora
-         break; 
-      } 
-      case API.WSL: { 
-        this.url = this.urlWsl; 
-         break; 
-      } 
-      case API.Local: { 
+    switch (api) {
+      case API.Infoflora:
+        this.url = this.urlInfoflora
+        break;
+      case API.WSL:
+        this.url = this.urlWsl;
+        break;
+      case API.Local:
         this.url = this.urlLocal;
-        break; 
-     } 
-      default: { 
-         break; 
-      } 
-   }
+        break;
+      default:
+        break;
+    }
 
   }
 
@@ -107,7 +124,7 @@ export class MainPage {
     this.url = this.urlLocal;
   }
 
-  getDate(): string {
+  setDate() {
     const today = new Date();
     const year = today.getFullYear();
     const month = (today.getMonth() + 1).toString().padStart(2, '0');
@@ -115,7 +132,7 @@ export class MainPage {
 
     const formattedDate = `${year}-${month}-${day}`;
     console.log(formattedDate);
-    return formattedDate;
+    this.date = formattedDate;
   }
 
   updateCoords() {
