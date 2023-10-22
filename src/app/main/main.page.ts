6b2cdb7ab25759eacce1c642961f4d4c24f85074
Manuel Brun <m_brun@msn.com>
2023-10-22 16:18:47 +0200

    localhost changes, files cleanup

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { PhotoService } from '../services/photo.service';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
@@ -8,29 +7,38 @@ import { Geolocation, Position } from '@capacitor/geolocation';
 import { FormsModule } from '@angular/forms';
 import { CapacitorHttp, HttpResponse } from '@capacitor/core';
 
+enum API {
+  Infoflora = 0,
+  WSL = 1,
+  Local = 2
+}
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss'],
+  selector: 'app-main',
+  templateUrl: 'main.page.html',
+  styleUrls: ['main.page.scss'],
   standalone: true,
-  imports: [IonicModule, ExploreContainerComponent, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
-export class Tab2Page {
+export class MainPage {
+  API = API;
+    
+  //  https -> android:networkSecurityConfig="@xml/network_security_config"
 
   public response: string = "";
   public num_taxon_id: number = 5;
   public req_taxon_id: number = 1046220;
-  public strawberries: string = "asdf";
-  public url: string = "asdf";
+  public api: API = API.Infoflora;
+  public host: string = "http://192.168.1.78";
+  public port: string = "1234";
+  public urlLocal: string = "http://127.0.0.1:8000/identify/images";
+  public urlInfoflora: string = "https://florid.infoflora.ch/api/v1/public/identify/images";
+  public urlWsl: string = "https://florid.infoflora.ch/api/v1/public/identify/images";
+  public url: string = this.urlInfoflora;
+  public requestInProgress: boolean = false;
   
   constructor(public photoService: PhotoService, private http: HttpClient) {
     this.updateCoords();
-  }
-
-  setUrl(url: string) {
-    console.log("asdfasdfasdf");
-    this.url = url;
   }
 
   addPhotoToGallery() {
@@ -50,31 +58,20 @@ export class Tab2Page {
     };
     console.log(body);
 
-    //const url = 'https://corsproxy.io/?' + encodeURIComponent('https://florid.infoflora.ch/api/v1/public/identify/images');
-    const url = 'http://192.168.1.78:1234/identify/images';
+    let requestUrl = [API.Infoflora, API.WSL].includes(this.api) ? 'https://corsproxy.io/?' + encodeURIComponent(this.url) : this.url;
 
     const options = {
-      url: url,
-      //url: 'https://cors-anywhere.herokuapp.com/https://florid.infoflora.ch/api/v1/public/identify/images',
-      //url: 'http://192.168.1.78:1234/identify/images',
-      //mode: 'no-cors',
+      url: requestUrl,
       headers: {'Content-Type': 'application/json'},
       data: body,
     };
   
+    this.requestInProgress = true;
     const response: HttpResponse = await CapacitorHttp.post(options);
+    this.requestInProgress = false;
 
     this.response = JSON.stringify(response, undefined, 2); 
     console.log(response);
-
-    /*this.http.post<any>('https://florid.infoflora.ch/api/v1/public/identify/images', body).subscribe(data => {
-      this.response = data;
-      console.log(data);
-    },
-    error => {
-      this.response = error.status + ", " + error.statusText;
-      console.log(error);
-  });*/
   }
 
   clear() {
@@ -82,6 +79,36 @@ export class Tab2Page {
     this.photoService.resizedPhotos.length = 0;
     this.photoService.base64Photos.length = 0;
     this.response = "";
+  }
+
+  apiChange(e: Event) {
+    console.log(e);
+    const api = (e as CustomEvent).detail.value;
+
+    switch(api) { 
+      case API.Infoflora: { 
+         this.url = this.urlInfoflora
+         break; 
+      } 
+      case API.WSL: { 
+        this.url = this.urlWsl; 
+         break; 
+      } 
+      case API.Local: { 
+        this.url = this.urlLocal;
+        //this.setLocalUrl();
+        break; 
+     } 
+      default: { 
+         break; 
+      } 
+   }
+
+  }
+
+  setLocalUrl() {
+    this.url = this.urlLocal;
+    //this.url = this.host + ":" + this.port + "/api/v1/public/identify/images";
   }
 
   getDate(): string {
--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -2,35 +2,25 @@ import { Routes } from '@angular/router';
 import { TabsPage } from './tabs.page';
 
 export const routes: Routes = [
-  {
-    path: 'tabs',
-    component: TabsPage,
-    children: [
-      {
-        path: 'tab1',
-        loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
-      },
-      {
-        path: 'tab2',
-        loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
-      },
-      {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
-      },
-      {
+    {
+        path: 'tabs',
+        component: TabsPage,
+        children: [
+            {
+                path: 'main',
+                loadComponent: () =>
+                    import('../main/main.page').then((m) => m.MainPage),
+            },
+            {
+                path: '',
+                redirectTo: '/tabs/main',
+                pathMatch: 'full',
+            },
+        ],
+    },
+    {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/main',
         pathMatch: 'full',
-      },
-    ],
-  },
-  {
-    path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full',
-  },
+    },
 ];
